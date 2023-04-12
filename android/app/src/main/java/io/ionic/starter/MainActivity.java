5f930e7e308b24e738825aeaa341b0148cea7289
unknown <vaishnavigulwelkar922@gmail.com>
2023-04-12 15:31:57 +0530

    java file change

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -2,4 +2,7 @@ package io.ionic.starter;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    System.out.println('Hello World');
+}
